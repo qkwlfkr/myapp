ea0c83abd5a942782e02e59c9aba4d65169e126c
qkwlfkr <80637634+qkwlfkr@users.noreply.github.com>
2021-04-16 11:46:17 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// ReactDOM.render(element, container[, callback])
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
